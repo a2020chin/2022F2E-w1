07e927d95a529cd0ea2c482cb31c346bc6917879
a2020chin <sshane258@gmail.com>
2022-11-03 13:16:37 +0800

    anime

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,71 @@
-import { useEffect , useRef } from 'react'
+import { useLayoutEffect , useRef } from 'react'
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
 
 
 
 function App() {
-  const loading = useRef();
 
-  useEffect(() => {
+
+  const tl = useRef();
+  const loading = useRef(null);
+  gsap.registerPlugin(TextPlugin);
+
+  useLayoutEffect(() => {
     
+    let ctx = gsap.context(() => {
+      tl.current = gsap.timeline({repeat: -1})
+        .to('.am_load span', {
+        duration: 1,
+        opacity: 0,
+        rotate: 180,
+        y: -25,
+        stagger: {
+          each: 0.25
+        },
+      }, ).to('.am_load span', {
+        duration: 1,
+        opacity: 1,
+        rotate: 360,
+        y: 0,
+        stagger: {
+          each: 0.25
+        },
+      }, '1')
+
+      gsap.to('.am_bar', {
+        duration: 0,
+        opacity: 1,
+        width: '0%',
+      })
+      gsap.to('.am_bar', {
+        duration: 3.5,
+        opacity: 1,
+        width: '100%',
+      })
+    }, loading)
+
+    return () => ctx.revert();
   },[])
 
 
   return (
     <div className=" bg-primaryGray-600 h-screen w-screen flex flex-col items-center justify-center" ref={loading}>
-        <h5 className='mb-4 text-textWhite'>LOADING...</h5>
+        <h5 className='am_load mb-4 text-textWhite flex'>
+          <span>L</span>
+          <span>O</span>
+          <span>A</span>
+          <span>D</span>
+          <span>I</span>
+          <span>N</span>
+          <span>G</span>
+          <span>.</span>
+          <span>.</span>
+          <span>.</span>
+        </h5>
         <div className='border-2 border-textWhite w-80 h-8 rounded'>
-          <div className='w-0 h-full bg-white'></div>
+          <div className='am_bar h-full bg-white'>
+          </div>
         </div>
     </div>
   )
